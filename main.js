f0e77fcc19e4958e5fd6efeb7ca83efa585915b9
s8 <konstantin.leo@gmail.com>
2023-02-07 07:39:21 +0200

    lesson 2 done

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+// const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Socks',
+            description: 'green socks'
+        }
+    }
+})
